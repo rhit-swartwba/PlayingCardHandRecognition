df21dc16af7ae7d8a81a57021ee59994ee4b73e3
Blaise Swartwood <swartwba@rose-hulman.edu>
2025-02-01 09:15:35 -0500

    Started cropping

---
+++ bounding_box_drawing.m
@@ -0,0 +1,26 @@
+imagePath = '../Results/combined_1_1.png';
+bboxFile = '../Results/combined_1_1.txt';
+
+img = imread(imagePath);
+
+fileID = fopen(bboxFile, 'r');
+bboxData = fscanf(fileID, '%d %f %f %f %f');
+fclose(fileID);
+
+x_center = bboxData(2);
+y_center = bboxData(3);
+width = bboxData(4);
+height = bboxData(5);
+
+[imgHeight, imgWidth, ~] = size(img);
+
+bboxWidth = round(width * imgWidth);
+bboxHeight = round(height * imgHeight);
+xMin = round((x_center * imgWidth) - (bboxWidth / 2));
+yMin = round((y_center * imgHeight) - (bboxHeight / 2));
+
+figure;
+imshow(img);
+hold on;
+rectangle('Position', [xMin, yMin, bboxWidth, bboxHeight], 'EdgeColor', 'r', 'LineWidth', 2);
+hold off;
